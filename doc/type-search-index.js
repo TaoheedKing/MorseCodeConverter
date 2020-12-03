5795ce9a31f78ba99e0d4862a35842b18429793d
TaoheedKing <tking92@montgomerycollege.edu>
2020-12-03 16:12:09 -0500

    finished product

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"MorseCodeConverter_GFA_Test"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"MorseCodeConverter"}]
